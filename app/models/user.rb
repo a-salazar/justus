d61a63bf162c52b266611adb7a4f40b4ad78a278
Charity Midenyo <cmidenyo@outlook.com>
2016-07-14 23:14:11 +0000

    added the post.html.erb view for thredded

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  #def admin
+   #has_attribute?(:admin)
+  #end
+  #def admin
+   #to_s == '#<User:0x007f90d4614920>'
+  #end
   def admin
-    has_attribute?(:admin)
+    :name == '#<User:0x007f90e0d608a8>'
   end
 end
